4b696ffe61e2e18cd454c93f08249294caee1c13
unknown <a.morais0101@gmail.com>
2025-05-27 19:18:41 +0100

    commit

--- handlers/login_callback.go
+++ handlers/login_callback.go
@@ -40,7 +40,7 @@ func CallbackHandler(c *gin.Context) {
 	fmt.Println("Session username:", session.Get("username"))
 	fmt.Println("Session avatar:", session.Get("avatar"))
 	fmt.Println("Session ID:", session.Get("id"))
-	c.Redirect(http.StatusFound, "https://agile-plan-backend.vercel.app/dashboard")
+	c.Redirect(http.StatusFound, "/dashboard")
 
 }
 
